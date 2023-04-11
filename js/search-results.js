af2537bdf305794b622c15a09c49549ad1dd9818
jleon80 <jleon80@outlook.com>
2023-04-11 10:53:29 +0200

    refactoring go to top button

--- js/search-results.js
+++ js/search-results.js
@@ -68,5 +68,27 @@ window.document.addEventListener("DOMContentLoaded", function () {
       console.log("Error: " + error);
     });
 
+  //-----button  to go to top on scroll-----
+  let mybutton = document.getElementById("goToTop");
+  mybutton.addEventListener("click", () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  });
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
 
 });
